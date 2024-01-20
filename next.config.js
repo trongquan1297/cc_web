1818510dca817feac410ba6fad4606c9e8b7788b
quann <quannt@appotapay.com>
2024-01-20 11:22:15 +0700

    fix: edit source image

--- next.config.js
+++ next.config.js
@@ -9,9 +9,8 @@ const nextConfig = {
     images: {
       remotePatterns: [
         {
-          protocol: 'http',
-          hostname: '127.0.0.1',
-          port: '1337',
+          protocol: 'https',
+          hostname: 'blog.quannguyen.fun',
           pathname: '/uploads/**',
         },
         {
