cf9b58b2d208419cb9b2f59ffee911c300f185c8
quann <quannt@appotapay.com>
2024-01-20 15:21:37 +0700

    fix: edit locate

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-    defaultLocale: 'en',
-    locales: ['en', 'de', 'cs'],
+    defaultLocale: 'vn',
+    locales: ['en', 'vn'],
 } as const;
 
 export type Locale = typeof i18n['locales'][number];
--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
           pathname: '/uploads/**',
         },
         {
+          protocol: 'http',
+          hostname: '127.0.0.1',
+          port: '1337',
+          pathname: '/uploads/**',
+        },
+        {
           protocol: 'https',
           hostname: 'images.pexels.com',
         }
