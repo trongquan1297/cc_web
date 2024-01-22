4b21b903e3799709a069075d923dac1fed124c11
quann <quannt@appotapay.com>
2024-01-22 18:26:21 +0700

    feat: add page

--- src/app/[lang]/components/AboutMe/AboutMe.tsx
+++ src/app/[lang]/components/AboutMe/AboutMe.tsx
@@ -8,20 +8,25 @@ const AboutMe: React.FC = ({}) => {
   
 
     return (
-      <div className="content">
-        <div className="content__container">
-          <p className="content__container__text">
-            Eveything in 
-          </p>
-          
-          <ul className="content__container__list">
-            <li className="content__container__list__item" id="devops">DevOps !</li>
-            <li className="content__container__list__item" id="guitar">Guitar !</li>
-            <li className="content__container__list__item" id="astro">Astronomy !</li>
-            <li className="content__container__list__item" id="photo">Photography !</li>
-          </ul>
+      <div>
+        <div className="hello">
+          <h1 className="hello_text">Hi !</h1>
         </div>
-</div>
+        <div className="content">
+          <div className="content__container">
+            <p className="content__container__text">
+              Eveything in 
+            </p>
+            
+            <ul className="content__container__list">
+              <li className="content__container__list__item" id="devops">DevOps !</li>
+              <li className="content__container__list__item" id="guitar">Guitar !</li>
+              <li className="content__container__list__item" id="astro">Astronomy !</li>
+              <li className="content__container__list__item" id="photo">Photography !</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
 };
 
