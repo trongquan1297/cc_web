048547e1bda5176a6713a60dca8a88ab26ce24d6
quann <quannt@appotapay.com>
2024-01-22 18:08:21 +0700

    feat: add page

--- src/app/[lang]/components/AboutMe/AboutMe.tsx
+++ src/app/[lang]/components/AboutMe/AboutMe.tsx
@@ -15,10 +15,10 @@ const AboutMe: React.FC = ({}) => {
           </p>
           
           <ul className="content__container__list">
-            <li className="content__container__list__item">DevOps !</li>
-            <li className="content__container__list__item">Guitar !</li>
-            <li className="content__container__list__item">Astronomy !</li>
-            <li className="content__container__list__item">Photography !</li>
+            <li className="content__container__list__item" id="devops">DevOps !</li>
+            <li className="content__container__list__item" id="guitar">Guitar !</li>
+            <li className="content__container__list__item" id="astro">Astronomy !</li>
+            <li className="content__container__list__item" id="photo">Photography !</li>
           </ul>
         </div>
 </div>
